0b86cc00de65cf77db0470b331680019bab3bc3d
Capella87 <contact@capelladev.com>
2022-12-17 16:02:03 +0900

    Update structure of the repository

---
+++ src/basic/server.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+
+int main(int argc, char** argv)
+{
+    printf("Hello Server\n");
+    return 0;
+}
---
+++ src/client-oriented/client.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
