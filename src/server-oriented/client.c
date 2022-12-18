f8cf66bdc1e141b879a86da4db421906b2558a0c
Capella87 <contact@capelladev.com>
2022-12-18 18:58:44 +0900

    Update src/server-oriented/client.c

--- src/server-oriented/client.c
+++ src/server-oriented/client.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/socket.h>
-#include <sys/shm.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
+#include <unistd.h>
 #include <sys/ipc.h>
-#include <time.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <fcntl.h>
 #include <sys/un.h>
+#include <stdbool.h>
 
 #define GROUP_NUM 4
-
 #define SOCKET_NAME "root"
 #define KB_1_INDEX 1024 * 1
 #define KB_1_SIZE 4 * 1024 * 1
 
-#define KB_64_INDEX 1024*64 
+#define KB_64_INDEX 1024*64
 #define KB_64_SIZE 4*1024*64
 
 #define KB_256_INDEX 1024*256 
 #define KB_256_SIZE 4*1024*256
-
-void handler()
-{
-    ;
-}
 
 typedef struct PACKET
 {
@@ -36,18 +30,12 @@ typedef struct PACKET
     int data[KB_1_INDEX];
 } packet;
 
-void createDataFile(char *filename, int *data)
+void handler(int signo)
 {
-
-    int out = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
-    write(out, data, sizeof(data));
-    close(out);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    system("clear");
-
     // for Manage signal 
     struct sigaction usrctrl;
     usrctrl.sa_flags = 0;
@@ -90,17 +78,16 @@ int main(int argc, char **argv)
 
     printf("Receive data from input\n");   
     packetSize = recv(clientId, (packet *)&in, sizeof(in), 0);
+    printf("%d %ld\n", in.dataIndex, in.dataSize);
 
-    for (i = 0; i < packetIndex;i++) {
-        
+    for (i = 0; i < in.dataIndex;i++) {
+        printf(" %d ", i);
         if (i % 4 == 0) data_0[i] = i;
         if (i % 4 == 1) data_1[i] = i;
         if (i % 4 == 2) data_2[i] = i;
         if (i % 4 == 3) data_3[i] = i;
+    } putchar('\n');
 
-    } 
-
-// 나중에 지워야 함
     printf("Create data file\n");
     createDataFile("data0", data_0);
     createDataFile("data1", data_1);
@@ -109,56 +96,180 @@ int main(int argc, char **argv)
 
     close(clientId);
 
-//
+    int my_number = 0;
+    sigaction(SIGUSR1, &usrctrl, (void*)0);    
+    pid_t children[4] = { getpid(), 0, 0, 0 };
 
-    key_t key;
-    int shmid;
 
-    // key = frok("shmfile",1);
-    shmid = shmget(0700, 1024, IPC_CREAT | 0666);
-    int shmid2 = shmget(0701, 1024, IPC_CREAT | 0666); // for pid
+    int server_shmid = shmget(123, sizeof(packet), IPC_PRIVATE);
+    packet* pkt = shmat(server_shmid, (void*)0, 0);
 
-    int *shmaddr2 = (int *)shmat(shmid2, NULL, 0); //
-    int parentpid = *(shmaddr2);                   //
 
-    // fork 4 compute node
-    for (int i = 0; i < 4; i++)
+    for (int i = 1; i < 4; i++)
     {
-        pid_t pid = fork();
-
-        if (pid == -1)
+        children[i] = fork();
+        
+        if (!children[i]) // child process
         {
-            perror("fork");
-            exit(-1);
+            my_number = i;
+            sigsuspend(&set);
+            break;
         }
-        if (pid == 0)
-        { // child process (compute node)
-
-            // each process get sorted, properly distributed data
-
-            // send to single file using shared memory
-            packet *shmaddr = shmat(shmid, NULL, 0);
-            char num[1024], buf[1024] = "This is compute node #";
-            
-            /*
-            sprintf(num, "%d", i);
-            strncat(buf, num, 1);
-            strncat(buf, "\n", 2);
-            strncat(shmaddr, buf, sizeof(buf));
-            shmdt(shmaddr);
-            exit(0);
-            */
+        else
+        {
         }
     }
 
-    for (int i = 0; i < 4; i++)
+    if (children[0] == getpid())
     {
-        pid_t pid = wait(NULL);
-        printf("Process %d is terminated.\n", pid);
-        /* code */
+        // Open a file
+        int fd = open("data0", O_RDONLY);
+        
+        int count = pkt->dataIndex / 4;
+        int temp;
+        
+        // Send to the shared memory
+        for (int i = 0; i < count; i++)
+        {
+            read(fd, &temp, sizeof(int));
+            pkt->data[i * 4] = temp;
+        }
+
+        close(fd);
+
+        for (int i = 1; i < 4; i++)
+        {
+            kill(children[i], SIGUSR1);
+        }
+        while (wait((void*)0) > 0);        // Send to the shared memory of server.
+
+        kill(atoi(argv[2]), SIGUSR1);
     }
-    // kill(atoi(argv[1]), SIGUSR1);
-    kill(parentpid, SIGUSR1); //
+    else
+    {
+        char name[6] = "data";
+        char numbername[2] = { (char)i + '0', '\0' };
+        strcat(name, numbername);
+
+        int fd = open(name, O_RDONLY);
+        
+        int count = pkt->dataIndex / 4;
+        int temp;
+        
+        // Send to the shared memory
+        for (int i = 0; i < count; i++)
+        {
+            read(fd, &temp, sizeof(int));
+            pkt->data[i * 4 + my_number] = temp;
+        }
+
+        close(fd);
+        return 0;
+    }
+    shmdt(pkt);
 
     return 0;
 }
+/*
+    struct sigaction act;
+    sigset_t set;
+    sigfillset(&set);
+    sigdelset(&set, SIGUSR1);
+    act.sa_handler = handler;
+    act.sa_flags = 0;
+    sigemptyset(&act.sa_mask);
+    int my_number = 0;
+
+    sigaction(SIGUSR1, &act, (void*)0);    
+    pid_t children[4] = { getpid(), 0, 0, 0};
+
+    // Open the server
+
+    int server_shmid = shmget(123, sizeof(collection), IPC_PRIVATE);
+    collection* serv_collection = shmat(server_shmid, (void*)0, 0);
+
+
+    for (int i = 1; i < 4; i++)
+    {
+        children[i] = fork();
+        
+        if (!children[i]) // child process
+        {
+            my_number = i;
+            sigsuspend(&set);
+            break;
+        }
+        else
+        {
+        }
+    }
+
+    if (children[0] == getpid())
+    {
+        int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+        struct sockaddr_un ar;
+        ar.sun_family = AF_UNIX;
+        strcpy(ar.sun_path, SOCKET_NAME);
+        int len = strlen(ar.sun_path) + sizeof(ar.sun_family);
+
+        if (connect(fd, (const struct sockaddr*)&ar, len) == -1)
+        {
+            perror("connect");
+            exit(1);
+        }
+        
+        packet* p = (packet*)malloc(sizeof(packet));
+        if (recv(fd, p, sizeof(packet), 0) == -1)
+        {
+            perror("recv");
+            exit(1);
+        }
+        close(fd);
+
+        // Send to the server.
+        memcpy(serv_collection->data, p->data, p->dataSize);
+        serv_collection->check--;
+
+        for (int i = 1; i < 4; i++)
+        {
+            kill(children[i], SIGUSR1);
+        }
+        while (wait((void*)0) > 0);        // Send to the shared memory of server.
+
+        kill(atoi(argv[2]), SIGUSR1);
+    }
+    else
+    {   
+        int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+        struct sockaddr_un ar;
+        ar.sun_family = AF_UNIX;
+        strcpy(ar.sun_path, SOCKET_NAME);
+        int len = strlen(ar.sun_path) + sizeof(ar.sun_family);
+
+        if (connect(fd, (const struct sockaddr*)&ar, len) == -1)
+        {
+            perror("connect");
+            exit(1);
+        }
+        
+        packet* p = (packet*)malloc(sizeof(packet));
+        if (recv(fd, p, sizeof(packet), 0) == -1)
+        {
+            perror("recv");
+            exit(1);
+        }
+        close(fd);
+
+        // Send to the server.
+        memcpy(serv_collection->data + my_number * (p->dataSize), p->data, p->dataSize);
+        serv_collection->check--;
+        printf("Node %d is done\n", my_number);
+
+        return 0;
+    }
+
+    shmdt(serv_collection);
+    shmctl(server_shmid, IPC_RMID, (void*)0);
+    return 0;
+}
+*/
